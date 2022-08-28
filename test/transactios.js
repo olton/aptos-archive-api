4ca53a9462360cec7f00f4b50a9518d60920ee33
olton <serhii@pimenov.com.ua>
2022-08-29 00:13:18 +0300

    rename connect.json to config.json

--- test/transactios.js
+++ test/transactios.js
@@ -2,4 +2,5 @@ import Archive from "../src/index.js";
 
 const arch = new Archive()
 
-console.log(await arch.transactionsFromAddress("0xbcf1a92d672640a70820f329c3923b56dd5d3368512b44a559f9d6d14c6d1973", {limit:2}))
+console.log(await arch.transactionsCount())
+// console.log(await arch.transactionsFromAddress("0xbcf1a92d672640a70820f329c3923b56dd5d3368512b44a559f9d6d14c6d1973", {limit:2}))
